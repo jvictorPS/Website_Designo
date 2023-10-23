90041c60aef29be0dbf5245e42a669db82b8b642
João Victor <j.victorbahia@gmail.com>
2023-10-23 20:30:17 -0300

    Adição da área de projetos do web design

--- src/pages/WebDesign/index.jsx
+++ src/pages/WebDesign/index.jsx
@@ -1,7 +1,8 @@
-import ProjectCard from "../../components/atoms/ProjectCard";
 import ServiceIntroductionCard from "../../components/atoms/ServiceIntroductionCard";
+import ProjectsSection from "../../components/molecules/ProjectsSection";
 
-import test from '../../assets/web-design/desktop/image-express.jpg'
+import projects from '../../data/projects.js'
+
 
 export default function WebDesign() {
     return (
@@ -11,11 +12,7 @@ export default function WebDesign() {
                 description={'We build websites that serve as powerful marketing tools and bring memorable brand experiences.'}
             />
 
-            <ProjectCard 
-                image={test}
-                title={'EXPRESS'}
-                description={'A multi-carrier shipping website for ecommerce businesses'}
-            />
+            <ProjectsSection projects={projects.web}/>
         
         </>
     )
