40a16311e0fc851313c5bde549d884ee87208eb9
João Victor <j.victorbahia@gmail.com>
2023-10-23 20:53:11 -0300

    dados dos projetos

---
+++ src/data/projects.js
@@ -0,0 +1,111 @@
+import web1 from '../assets/web-design/desktop/image-express.jpg'
+import web2 from '../assets/web-design/desktop/image-transfer.jpg'
+import web3 from '../assets/web-design/desktop/image-photon.jpg'
+import web4 from '../assets/web-design/desktop/image-builder.jpg'
+import web5 from '../assets/web-design/desktop/image-blogr.jpg'
+import web6 from '../assets/web-design/desktop/image-camp.jpg'
+
+import app1 from '../assets/app-design/desktop/image-airfilter.jpg'
+import app2 from '../assets/app-design/desktop/image-eyecam.jpg'
+import app3 from '../assets/app-design/desktop/image-faceit.jpg'
+import app4 from '../assets/app-design/desktop/image-todo.jpg'
+import app5 from '../assets/app-design/desktop/image-loopstudios.jpg'
+
+import graphic1 from '../assets/graphic-design/desktop/image-change.jpg'
+import graphic2 from '../assets/graphic-design/desktop/image-boxed-water.jpg'
+import graphic3 from '../assets/graphic-design/desktop/image-science.jpg'
+
+const ProjectData = {
+        web: [
+            {
+                "id": 0,
+                "image": web1,
+                "title": "EXPRESS",
+                "description": "A multi-carrier shipping website for ecommerce businesses"
+            },
+            {
+                "id": 1,
+                "image": web2,
+                "title": "TRANSFER",
+                "description": "Site for low-cost money transfers and sending money within seconds"
+            },
+            {
+                "id": 2,
+                "image": web3,
+                "title": "PHOTON",
+                "description": "A state-of-the-art music player with high-resolution audio and DSP effects"
+            },
+            {
+                "id": 3,
+                "image": web4,
+                "title": "BUILDER",
+                "description": "Connects users with local contractors based on their location"
+            },
+            {
+                "id": 4,
+                "image": web5,
+                "title": "BLOGR",
+                "description": "Blogr is a platform for creating an online blog or publication"
+            },
+            {
+                "id": 5,
+                "image": web6,
+                "title": "CAMP",
+                "description": "Get expert training in coding, data, design, and digital marketing"
+            }
+        ],
+        app: [
+            {
+                "id": 0,
+                "image": app1,
+                "title": "AIRFILTER",
+                "description": "Solving the problem of poor indoor air quality by filtering the air"
+            },
+            {
+                "id": 1,
+                "image": app2,
+                "title": "EYECAM",
+                "description": "Product that lets you edit your favorite photos and videos at any time"
+            },
+            {
+                "id": 2,
+                "image": app3,
+                "title": "FACEIT",
+                "description": "Get to meet your favorite internet superstar with the faceit app"
+            },
+            {
+                "id": 3,
+                "image": app4,
+                "title": "TODO",
+                "description": "A todo app that features cloud sync with light and dark mode"
+            },
+            {
+                "id": 4,
+                "image": app5,
+                "title": "LOOPSTUDIOS",
+                "description": "A VR experience app made for Loopstudios"
+            },
+        ],
+        graphic: [
+            {
+                "id": 0,
+                "image": graphic1,
+                "title": "TIM BROWN",
+                "description": "A book cover designed for Tim Brown’s new release, ‘Change’"
+            },
+            {
+                "id": 1,
+                "image": graphic2,
+                "title": "BOXED WATER",
+                "description": "A simple packaging concept made for Boxed Water"
+            },
+            {
+                "id": 2,
+                "image": graphic3,
+                "title": "SCIENCE!",
+                "description": "A poster made in collaboration with the Federal Art Project"
+            },
+        ]
+}
+
+export default ProjectData
