4fbaf516fcf4f60efcd430e1bc90856f7b9d4d74
João Victor <j.victorbahia@gmail.com>
2023-10-24 22:37:21 -0300

    teste bug icon Marker do CardMap

--- src/components/atoms/MapCard/index.jsx
+++ src/components/atoms/MapCard/index.jsx
@@ -3,6 +3,11 @@
 import { Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css"
 import { MapContainerStyled } from "./style";
+import { Icon } from "leaflet";
+
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 export default function MapCard({
     latitude,
@@ -16,10 +21,19 @@ export default function MapCard({
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[latitude, longitude]}>
-                <Popup>
-                    {office}
-                </Popup>
+            <Marker 
+                position={[latitude, longitude]}
+                icon={
+                    new Icon({
+                        iconRetinaUrl: markerIcon2x,
+                        iconUrl: markerIcon,
+                        shadowUrl: markerShadow,
+                    })
+                }
+                >
+                    <Popup>
+                        {office}
+                    </Popup>
             </Marker>
         </MapContainerStyled>
     )
